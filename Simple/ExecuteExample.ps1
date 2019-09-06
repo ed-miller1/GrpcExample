22a83584df44cd632562d2533c9818fb2756f4a0
Ed Miller <emiller@rightpoint.com>
2019-09-06 14:53:55 -0500

    update simple execution script and fix python bug

---
+++ Simple/ExecuteExample.ps1
@@ -0,0 +1,11 @@
+﻿#run dotnet core web apps for rest/grpc demo
+#will assume user has .net core 3 and VS 2019 latest preview
+#script will live in GrpcTests root folder and should be executed from there
+
+#Build and run Service
+dotnet restore ./GrpcExampleService/
+dotnet build ./GrpcExampleService/GrpcExampleService.csproj
+start powershell {dotnet run --project ./GrpcExampleService/GrpcExampleService.csproj; Read-Host }
+
+#Run client
+py ./PythonClient/PersonClient.py localhost:5001 5000 True
--- Simple/PythonClient/PersonClient.py
+++ Simple/PythonClient/PersonClient.py
@@ -11,13 +11,14 @@ import sys
 def run_grpc(grpc_host, grpc_loops, print_results):
     channel = grpc.insecure_channel(grpc_host)
     stub = person_pb2_grpc.PersonServStub(channel)
-    for x in range(1,grpc_loops):
+    for x in range(1,int(grpc_loops)):
         grpc_response = stub.GetPerson(person_pb2.GetPersonRequest(id = x))
         if print_results:
             print("Person client received " + str(grpc_response.id) + " " + grpc_response.name + " " + grpc_response.email)
 
 def run():
-    if(len(sys.argv) == 3):
+    print("Number of args: " + str(len(sys.argv)))
+    if(len(sys.argv) == 4):
         grpc_host = sys.argv[1]
         grpc_loops = sys.argv[2]
         print_results = sys.argv[3]
